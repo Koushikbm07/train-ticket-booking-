146801d9e43985d5d041645d80af27d278ed1da1
koushik <kmalagannavar@gmail.com>
2026-03-17 22:56:56 +0530

    Project Initialization

---
+++ app/src/main/java/org/example/service/UserBookingService.java
@@ -0,0 +1,12 @@
+package org.example.service;
+
+import org.example.entity.User;
+
+public class UserBookingService {
+
+    private User user;
+
+    public UserBookingService(User user) {
+        this.user = user;
+    }
+}
